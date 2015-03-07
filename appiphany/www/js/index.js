464d491f7753626a4ac50b3944e5b465418811cd
MikeC1995 <mc13818@my.bristol.ac.uk>
2015-03-07 20:05:14 +0000

    closes to center markers popup, added basic menu functionality

--- appiphany/www/js/index.js
+++ appiphany/www/js/index.js
@@ -5,12 +5,11 @@ var map;
 
 var accToken = '?access_token=pk.eyJ1IjoibWMxMzgxOCIsImEiOiI4Tlp2cFlBIn0.reMspV4lEYawDlSZ6U1fqQ';
 var markers = new Array();
+var leaflet_markers = new Array();
 
 map = L.map('map-layer', {
     attributionControl: false,
     zoomControl:false,
-    //center: [51.45, -2.6],
-    //zoom: 15,
     center: [51.396, -2.298],
     zoom: 14,
     minZoom: 8
@@ -23,6 +22,7 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v4/mc13818.l2a71g35/{z}/{x}/{y}.png'.co
 		unloadInvisibleTiles: false
 }).addTo(map);
 
+//custom marker 
 var markerIcon = L.icon({
     iconUrl: 'img/marker.png',
     iconSize: [25, 25],
@@ -31,23 +31,75 @@ var markerIcon = L.icon({
 });
 
 map.on('click', function(e) {
-    createMarker(e.latlng.lat, e.latlng.lng, "Hello, world!");
+    createMarker(e.latlng.lat, e.latlng.lng, "Mike Christensen", "Hello, World!");
 });
 
-function createMarker(latitude, longitude, message) {
+map.on('move', popupCenterMarker);
+
+function createMarker(latitude, longitude, _name, message) {
+  leaflet_m = addMarkerToMap(latitude, longitude, _name);
   var m = {
     lat: latitude,
     lng: longitude,
-    msg: message
+    name: _name,
+    msg: message,
+    leaflet_marker: leaflet_m
   };
   markers.push(m);
-  addMarkerToMap(m.lat, m.lng, m.msg);
 }
 
-function addMarkerToMap(lat, lng, message) {
+function addMarkerToMap(lat, lng, name) {
   m = L.marker([lat, lng], {icon: markerIcon});
   m.addTo(map)
-    .bindPopup('<div class="popup">'+message+'<\div>')
+    .bindPopup('<div class="popup"><div id = "profile_img"><img src="img/profiles/profile1.jpg"></div><div id = "profile_name">'+name+'</div></div>')
     .openPopup();
+  return m;
 }
 
+function popupCenterMarker() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].leaflet_marker.closePopup();
+  }
+  getCenterMarker().leaflet_marker.openPopup();
+}
+
+function calcDistance(p1, p2)  {
+  //http://www.movable-type.co.uk/scripts/latlong.html
+  var R = 6371000; // metres (radius of Earth)
+  var phi1 = toRadians(p1.lat);
+  var phi2 = toRadians(p2.lat);
+  var delta_phi = toRadians(p2.lat-p1.lat);
+  var delta_lambda = toRadians(p2.lng-p1.lng);
+
+  var a = Math.sin(delta_phi/2) * Math.sin(delta_phi/2) +
+          Math.cos(phi1) * Math.cos(phi2) *
+          Math.sin(delta_lambda/2) * Math.sin(delta_lambda/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d;
+}
+
+function getCenterMarker() {
+  var center = map.getCenter();
+  var min_i = 0;
+  var min_dist = calcDistance(center,markers[0]);
+  for (var i = 1; i < markers.length; i++) {
+    var dist = calcDistance(center,markers[i]);
+    if(dist < min_dist) {
+      min_dist = dist;
+      min_i = i;
+    }
+  }
+  return markers[min_i];
+}
+
+// Converts from degrees to radians.
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+$('#expand-icon').click(function() {
+  $('#message-banner').css('display','none');
+  $('#message-layer').css('display','block');
+  $('#map-layer').css('height', '40%');
+});
