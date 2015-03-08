6d8e8c835af35d16345f747fed11a18e7c17fdc4
MikeC1995 <mc13818@my.bristol.ac.uk>
2015-03-08 01:11:42 +0000

    added submit message page

--- appiphany/www/js/index.js
+++ appiphany/www/js/index.js
@@ -34,10 +34,9 @@ map.on('click', function(e) {
     createMarker(e.latlng.lat, e.latlng.lng, "Mike Christensen", "Hello, World!");
 });
 
-map.on('moveend', popupCenterMarker);
-
 map.on('moveend', function(){
-    getData();
+    //getData();
+    popupCenterMarker();
 });
 
 function createMarker(latitude, longitude, _name, type, message, id) {
@@ -136,16 +135,34 @@ function toRadians(degrees) {
   return degrees * Math.PI / 180;
 };
 
-$('#expand-icon').click(function() {
-  if($('#expand-icon img').attr('src') == 'img/down.png') {
-    $('#message-banner').css('display','none');
-    $('#message-layer').slideDown();
+$('#control-icon').click(function() {
+  if($('#control-icon img').attr('src') == 'img/down.png') {
+    $('#banner-layer').css('display','none');
+    $('#view-message-layer').slideDown();
     $('#map-layer').css('height', '40%');
-    $('#expand-icon img').attr('src', 'img/up.png');
-  } else {
-    $('#message-banner').slideDown();
-    $('#message-layer').slideUp();
+    $('#control-icon img').attr('src', 'img/up.png');
+  } else if($('#control-icon img').attr('src') == 'img/up.png') {
+    $('#banner-layer').slideDown();
+    $('#view-message-layer').slideUp();
     $('#map-layer').css('height', 'calc(100% - 75px)');
-    $('#expand-icon img').attr('src', 'img/down.png');
+    $('#control-icon img').attr('src', 'img/down.png');
+  } else {
+    $('#add-message-layer').slideUp();
+    $('#banner-layer').slideDown();
+    $('#button-layer').fadeIn();
+    $('#control-icon img').attr('src','img/down.png');
   }
+});
+
+$('#button-add').click(function() {
+  if($('#control-icon img').attr('src') == 'img/up.png') {
+    $('#banner-layer').slideDown();
+    $('#view-message-layer').slideUp();
+    $('#map-layer').css('height', 'calc(100% - 75px)');
+    $('#control-icon img').attr('src', 'img/down.png');
+  }
+  $('#add-message-layer').slideDown();
+  $('#banner-layer').css('display','none');
+  $('#button-layer').fadeOut();
+  $('#control-icon img').attr('src','img/close.png');
 });
